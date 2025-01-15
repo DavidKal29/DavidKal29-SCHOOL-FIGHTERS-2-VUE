933d966e696aef6ac3aca4745796873585d2efe9
David Kalmuk <correo@correo.com>
2025-01-15 08:56:58 +0100

    Hacemos que se pueda elegir escenario y guardarlo en el localsotrage

--- school-fighters-2/src/views/VistaEscenarios.vue
+++ school-fighters-2/src/views/VistaEscenarios.vue
@@ -6,20 +6,45 @@
             escenario donde ocurrirá la batalla</p>
       </div>
       <div class="flex gap-[20px] justify-center items-center">
-          <select class="bg-white rounded-[7px] py-[16px] px-[10vh] text-black text-[20px] font-bold">
+          <select class="bg-white rounded-[7px] py-[16px] px-[10vh] text-black text-[20px] font-bold" v-model="escenario">
               <option value="">Elige un escenario</option>
+              <option v-for="(esc,index) in escenas" :key="index" :value="esc.nombre">{{ esc.nombre }}</option>
           </select>
       </div>
       <div class="flex justify-center items-center gap-[30px]">
         <router-link to="/"><button class=" text-[20px] py-[12px] bg-[#BC1E1E] rounded-[7px] px-[16vh] font-bold">Volver</button></router-link>
-        <router-link to="/combate"><button class=" text-[20px] py-[12px] bg-[#6F1E72] rounded-[7px] px-[12vh] font-bold">Continuar</button></router-link>
+        <router-link to="/combate"><button class=" text-[20px] py-[12px] bg-[#6F1E72] rounded-[7px] px-[12vh] font-bold" @click="guardarEscenario">Continuar</button></router-link>
       </div>
     </section>
   </template>
 
 <script>
+import { escenarios } from '../db/datos.js';
 export default {
-    name:'VistaEscenarios'
+    name:'VistaEscenarios',
+    data(){
+      return {
+        escenas:'',
+        escenario:''
+      }
+    },
+    methods:{
+      getLocalSotrageEscenarios(){
+        this.escenas=JSON.parse(localStorage.getItem('escenarios'))
+        if (this.escenas==null) {
+            this.escenas=escenarios
+        }
+        localStorage.setItem('escenarios',JSON.stringify(this.escenas))
+      },
+      guardarEscenario(){
+        this.escenario=this.escenas.find(e=>e.nombre===this.escenario)
+
+        localStorage.setItem('escenario',JSON.stringify(this.escenario))
+      }
+    },
+    mounted(){
+      this.getLocalSotrageEscenarios()
+    }
 }
 </script>
 

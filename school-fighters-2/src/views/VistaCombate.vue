d2456a569206c83272f57a548377909c63c4c75d
David Kalmuk <correo@correo.com>
2025-01-15 09:03:04 +0100

    Hacemos que el escenario elegido, salga en la vista combate

--- school-fighters-2/src/views/VistaCombate.vue
+++ school-fighters-2/src/views/VistaCombate.vue
@@ -1,17 +1,24 @@
 <template>
-  <section :style="{ backgroundImage: `url('${this.imagen}')` }" class="h-screen bg-cover">
+  <section :style="{ backgroundImage: `url('${this.escenario.imagen}')` }" class="h-screen bg-cover">
     <router-link to="/"><button class="bg-[#194988] text-white rounded-[7px] py-[5px] px-[30px] m-[10px]">Inicio</button></router-link>
   </section>
 </template>
 
 <script>
-import { escenarios } from '../db/datos.js'
 export default {
     name:'VistaCombate',
     data(){
         return{
-            imagen:escenarios[1].imagen
+            escenario:''
         }
+    },
+    methods:{
+        getEscenario(){
+          this.escenario=JSON.parse(localStorage.getItem('escenario'))
+        }
+    },
+    mounted(){
+      this.getEscenario()
     }
 }
 </script>
